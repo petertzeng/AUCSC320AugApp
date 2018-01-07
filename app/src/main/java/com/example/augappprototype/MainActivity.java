eaed0ac7fc013af3c2f6aab614218aeca3e90652
pao2o <32780758+pao2o@users.noreply.github.com>
2018-01-07 05:24:45 -0700

    made a popup window

--- app/src/main/java/com/example/augappprototype/MainActivity.java
+++ app/src/main/java/com/example/augappprototype/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.augappprototype;
 
+import android.app.Dialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 public class MainActivity extends AppCompatActivity implements DayViewDecorator {
 
@@ -16,14 +21,28 @@ public class MainActivity extends AppCompatActivity implements DayViewDecorator 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MaterialCalendarView materialCalendar = (MaterialCalendarView)findViewById(R.id.calendarView);
+        MaterialCalendarView materialCalendar = findViewById(R.id.calendarView);
         materialCalendar.state().edit()
                 .setMinimumDate(CalendarDay.from(2017, 12, 31))
                 .setMaximumDate(CalendarDay.from(2018, 12, 31))
                 .commit();
-        materialCalendar.addDecorators(new CurrentDayDecorator(this));
-    }
+       // materialCalendar.addDecorators(new CurrentDayDecorator(this));
 
+        materialCalendar.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                final Dialog d = new Dialog(MainActivity.this);
+                d.setContentView(R.layout.eventpopup);
+                d.show();
+                Button close_btn = d.findViewById(R.id.bottom);
+                close_btn.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        d.dismiss();
+                    }
+                });
+            }
+        });
+    }
 
     @Override
     public boolean shouldDecorate(CalendarDay day) {
