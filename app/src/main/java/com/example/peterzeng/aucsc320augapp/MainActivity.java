d3c3ef3c8499fdb544f83a057905718ae9d98dc6
Peter Zeng <shichun1@ualberta.ca>
2018-01-08 14:33:14 -0700

    Initialized a bunch of local variables to hide the logged in status indicator until the user logs in.

--- app/src/main/java/com/example/peterzeng/aucsc320augapp/MainActivity.java
+++ app/src/main/java/com/example/peterzeng/aucsc320augapp/MainActivity.java
@@ -4,17 +4,34 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener,
     GoogleApiClient.OnConnectionFailedListener {
 
+  private LinearLayout profileSection;
+  private Button signOut;
+  private SignInButton signIn;
+  private TextView name;
+  private TextView profilePicture;
+  private GoogleApiClient googleApiClient;
+  private static final int REQUEST_CODE = 9001;
+
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    profileSection = (LinearLayout)findViewById(R.id.profile_section);
+    signOut = (Button)findViewById(R.id.logout_button);
+    signIn = (SignInButton)findViewById(R.id.login_button);
+
   } // onCreate(Bundle)
 
   @Override
@@ -26,4 +43,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
   public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
   } // onConnectionFailed(ConnectionResult)
+
 } // MainActivity
