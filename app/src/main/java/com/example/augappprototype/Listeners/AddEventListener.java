49620b2a49f642652960f65d05fa79054a8b6a0c
pao2o <vpreyes@ualberta.ca>
2018-01-08 17:03:12 -0700

    trying to get time input

--- app/src/main/java/com/example/augappprototype/Listeners/AddEventListener.java
+++ app/src/main/java/com/example/augappprototype/Listeners/AddEventListener.java
@@ -2,6 +2,9 @@ package com.example.augappprototype.Listeners;
 
 import android.app.Dialog;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.augappprototype.MainActivity;
@@ -18,7 +21,10 @@ import java.util.HashMap;
 public class AddEventListener implements View.OnClickListener {
     public static HashMap<Date, ArrayList<String>> events = new HashMap<>();
     ArrayList<String> eventDetails = new ArrayList();
-
+    private int day;
+    private int month;
+    private int year;
+    private String step;
     private final MainActivity mainActivity;
 
 
@@ -28,10 +34,11 @@ public class AddEventListener implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        addEvents();
+        selectDate();
+
     }
 
-    public void addEvents(){
+    public void selectDate(){
         /**
         eventDetails.add(0, "Augustana");
         eventDetails.add(1, "Basketball Game");
@@ -44,6 +51,66 @@ public class AddEventListener implements View.OnClickListener {
         final Dialog addEventDialog = new Dialog(mainActivity);
         addEventDialog.setContentView(R.layout.addeventpopup);
         addEventDialog.show();
+        step = "date";
+        closeWindowListener(addEventDialog);
+        continueButtonListener(addEventDialog);
+    }
+
+    public void closeWindowListener(final Dialog addEvents){
+        Button closeWindow = addEvents.findViewById(R.id.closeWindow);
+        closeWindow.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                addEvents.dismiss();
+            }
+        });
+    }
+
+    public void continueButtonListener(final Dialog addEvents){
+        Button continueButton = addEvents.findViewById(R.id.continueAdd);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+        DatePicker eventDate = addEvents.findViewById(R.id.datePicker);
+        TimePicker eventTime = addEvents.findViewById(R.id.timePicker);
+            @Override
+            public void onClick(View v) {
+                if (step == "date") {
+                    year = eventDate.getYear();
+                    month = eventDate.getMonth();
+                    day = eventDate.getDayOfMonth();
+                    addEvents.dismiss();
+                    openEventDetails();
+                    step = "time";
+                }
+                else if (step == "time"){
+                    int minute = eventTime.getCurrentMinute();
+                    int hour = eventTime.getCurrentHour();
+                    addEvents.dismiss();
+
+
+                }
+            }
+        });
+
+    }
+
+    public void openEventDetails(){
+        final Dialog addEventDialog = new Dialog(mainActivity);
+        addEventDialog.setContentView(R.layout.addeventtime);
+        addEventDialog.show();
+    }
+
+
+    public int getMonth(){
+        return month;
+    }
+
+    public int getDay(){
+        return day;
+    }
+
+    public int getYear(){
+        return year;
     }
 
     public HashMap getEvents(){
