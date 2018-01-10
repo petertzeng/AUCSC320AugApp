82f802a2106eec43b013cd6754e502da5f931c98
ChaseLarocque <32780762+ChaseLarocque@users.noreply.github.com>
2018-01-10 11:48:42 -0700

    Added listeners for the eventCalendar button. Changed guest login to go to main menu. Changed colours of the main menu button.

--- app/src/main/java/com/example/augappprototype/Listeners/GuestButtonListener.java
+++ app/src/main/java/com/example/augappprototype/Listeners/GuestButtonListener.java
@@ -5,7 +5,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.augappprototype.LoginScreen;
-import com.example.augappprototype.MainActivity;
+import com.example.augappprototype.MainMenu;
 
 /**
  * Created by Pao on 1/7/2018.
@@ -22,8 +22,8 @@ public class GuestButtonListener implements View.OnClickListener{
     public void onClick(View v) {
         Toast.makeText(loginScreen, "Logged in as a guest",
                 Toast.LENGTH_LONG).show();
-        Intent k = new Intent(loginScreen, MainActivity.class);
-        loginScreen.startActivity(k);
+        Intent goToMainMenu = new Intent(loginScreen, MainMenu.class);
+        loginScreen.startActivity(goToMainMenu);
     }
 
 
--- app/src/main/java/com/example/augappprototype/MainMenu.java
+++ app/src/main/java/com/example/augappprototype/MainMenu.java
@@ -3,11 +3,15 @@ package com.example.augappprototype;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.augappprototype.Listeners.EventCalendarListener;
+
 public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        findViewById(R.id.eventsCalendarImage).setOnClickListener(new EventCalendarListener(this));
+
     }
 }
